8b703bd33202da5a4afced32b3315937f05a5cce
GSOFelix <guilhermedesoares@gmail.com>
2025-05-07 17:50:32 -0300

    feat: adicionar context e configurações das entidades

---
+++ Context/EntitiesConfigurations/MainTaskConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskFlow.Domain.Entities;
+
+namespace TaskFlow.Context.EntitiesConfigurations
+{
+    public class MainTaskConfiguration : IEntityTypeConfiguration<MainTask>
+    {
+        public void Configure(EntityTypeBuilder<MainTask> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Title).IsRequired().HasMaxLength(70);
+            builder.Property(x => x.Description).IsRequired().HasMaxLength(500);
+            builder.Property(x => x.Status).IsRequired();
+            builder.Property(x => x.CreatedAt).IsRequired().HasColumnType("TIMESTAMP WITH TIME ZONE");
+        }
+    }
+}
---
+++ Context/EntitiesConfigurations/UserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskFlow.Domain.Entities;
+
+namespace TaskFlow.Context.EntitiesConfigurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(128);
+            builder.Property(x => x.Email).IsRequired();
+            builder.Property(x => x.PasswordHash).IsRequired();
+            builder.Property(x => x.CreatAt).IsRequired().HasColumnType("TIMESTAMP WITH TIME ZONE");
+        }
+    }
+}

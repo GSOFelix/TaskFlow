cd1484fedc5c7a289244f46f4195d8a57b07e69d
GSOFelix <guilhermedesoares@gmail.com>
2025-05-08 15:14:35 -0300

    feat: criar implementações dos repositorios

---
+++ Repositories/MainTaskRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskFlow.Context;
+using TaskFlow.Domain.Entities;
+using TaskFlow.Domain.Interfaces.Repository;
+
+namespace TaskFlow.Repositories
+{
+    public class MainTaskRepository : IMainTaskRepository
+    {
+        private readonly AppDbContext _context;
+
+        public MainTaskRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<MainTask>> GetAllByUserAsync(long userId, CancellationToken token)
+        {
+            return await _context.MainTasks.Where(u => u.UserId == userId).ToListAsync(token);
+        }
+
+        public async Task<MainTask?> GetByIdAsync(long mainTaskId, CancellationToken token)
+        {
+            return await _context.MainTasks.FirstOrDefaultAsync(m => m.Id == mainTaskId, token);
+        }
+
+        public async Task<long> InsertAsync(MainTask mainTask, CancellationToken token)
+        {
+            await _context.MainTasks.AddAsync(mainTask, token);
+            return await _context.SaveChangesAsync(token);
+        }
+    }
+}

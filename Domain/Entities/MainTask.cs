4248f671e2733d4d119e7568829767a72f6c8841
GSOFelix <guilhermedesoares@gmail.com>
2025-05-08 17:03:27 -0300

    feat: adicionar validação de dominio nas entidades

--- Domain/Entities/MainTask.cs
+++ Domain/Entities/MainTask.cs
@@ -1,4 +1,5 @@
 ﻿using TaskFlow.Domain.Enums;
+using TaskFlow.Domain.Exceptions;
 
 namespace TaskFlow.Domain.Entities
 {
@@ -15,11 +16,14 @@ namespace TaskFlow.Domain.Entities
         public List<Subtask> Subtasks { get; set; } = [];
         public List<Comment> Comments { get; set; } = [];
 
-        public MainTask(string title, string description, ETaskStatus status, long userId)
+        public MainTask(string title, string description, long userId)
         {
+            DomainRule.When(string.IsNullOrWhiteSpace(title), "O Titulo é obrigatorio");
             Title = title;
+            DomainRule.When(string.IsNullOrWhiteSpace(description), "A Descrição é obrigatorio");
             Description = description;
-            Status = status;
+            Status = ETaskStatus.ToDo;
+            DomainRule.When(userId <= 0, "Id de usuário invalida");
             UserId = userId;
             CreatedAt = DateTime.UtcNow;
         }
@@ -36,12 +40,15 @@ namespace TaskFlow.Domain.Entities
         public ETaskStatus Status { get; set; }
         public DateTime CreatedAt { get; set; }
 
-        public Subtask(long mainTaskId, string title, string description, ETaskStatus status)
+        public Subtask(long mainTaskId, string title, string description)
         {
+            DomainRule.When(mainTaskId <= 0, "Id da Tarefa inválido");
             MainTaskId = mainTaskId;
+            DomainRule.When(string.IsNullOrWhiteSpace(title), "O Titulo é obrigatorio");
             Title = title;
+            DomainRule.When(string.IsNullOrWhiteSpace(description), "A Descrição é obrigatorio");
             Description = description;
-            Status = status;
+            Status = ETaskStatus.ToDo;
             CreatedAt = DateTime.UtcNow;
         }
     }
@@ -49,19 +56,27 @@ namespace TaskFlow.Domain.Entities
     public class Comment
     {
         public long Id { get; set; }
-        public long UserId { get; set; }
+        public long UserId { get; private set; }
         public User User { get; set; } = null!;
-        public long MainTaskId { get; set; }
+        public long MainTaskId { get; private set; }
         public MainTask MainTask { get; set; } = null!;
-        public string Text { get; set; } = null!;
-        public DateTime CreatedAt { get; set; }
+        public string Text { get; private set; } = null!;
+        public DateTime CreatedAt { get; private set; }
 
         public Comment(long userId, long mainTaskId, string text)
         {
+            DomainRule.When(userId <= 0, "Id do Usuário inválido");
             UserId = userId;
+            DomainRule.When(mainTaskId <= 0, "Id da Tarefa inválido");
             MainTaskId = mainTaskId;
+            DomainRule.When(string.IsNullOrWhiteSpace(text), "O Texto é obrigatorio");
             Text = text;
             CreatedAt = DateTime.UtcNow;
+        }
+
+        public void UpdateText(string newText)
+        {
+            Text = newText;
         }
     }
 
@@ -76,7 +91,9 @@ namespace TaskFlow.Domain.Entities
 
         public TaskAssignee(long userId, long mainTaskId)
         {
+            DomainRule.When(userId <= 0, "Id do Usuário inválido");
             UserId = userId;
+            DomainRule.When(mainTaskId <= 0, "Id da Tarefa inválido");
             MainTaskId = mainTaskId;
         }
     }
--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,4 +1,6 @@
-﻿namespace TaskFlow.Domain.Entities
+﻿using TaskFlow.Domain.Exceptions;
+
+namespace TaskFlow.Domain.Entities
 {
     public class User
     {
@@ -13,10 +15,18 @@
 
         public User(string name, string email, string passWordHash)
         {
-            Name = name;
+            DomainValidation(name, passWordHash); 
             Email = email;
-            PasswordHash = passWordHash;
             CreatAt = DateTime.UtcNow;
+        }
+
+        private void DomainValidation(string name, string passWordHash)
+        {
+            DomainRule.When(string.IsNullOrWhiteSpace(name), "O nome é obrigatorio");
+            DomainRule.When(name.Length < 3 , "O nome deve ter no mínimo 3 caracteres");
+            Name = name;
+            DomainRule.When(string.IsNullOrWhiteSpace(passWordHash), "A senha é obrigatoria");
+            PasswordHash = passWordHash;
         }
 
 

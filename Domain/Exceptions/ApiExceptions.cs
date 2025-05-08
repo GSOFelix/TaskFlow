16e3ce446b97ef10b9544246cc1aa5d73eb39685
GSOFelix <guilhermedesoares@gmail.com>
2025-05-08 17:02:46 -0300

    feat: Incluir Exceções padronizadas

---
+++ Domain/Exceptions/ApiExceptions.cs
@@ -0,0 +1,19 @@
+﻿namespace TaskFlow.Domain.Exceptions
+{
+    public class NotFoundException(string message) : Exception(message) { }
+    public class BadRequestException(string message) : Exception(message) { }
+    public class Unauthorizeexception(string message) : Exception(message) { }
+    public class InternalErrorException(string message, Exception? inner) : Exception(message, inner) { }
+
+    public class DomainRule(string message) : Exception(message)
+    {
+        public static void When(bool hasError, string message)
+        {
+            if (hasError)
+            {
+                throw new BadRequestException(message);
+            }
+        }
+    }
+
+}

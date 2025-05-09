bf14629caf793828f565cfa3248ae9f3da319bd6
GSOFelix <guilhermedesoares@gmail.com>
2025-05-09 17:31:18 -0300

    fest: criar UseCase de User e seu contrato

---
+++ Application/UseCases/Implementations/UserUseCase.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using TaskFlow.Aplication.Dtos;
+using TaskFlow.Application.Dtos;
+using TaskFlow.Application.Mappers;
+using TaskFlow.Application.UseCases.Interfaces;
+using TaskFlow.Domain.Entities;
+using TaskFlow.Domain.Exceptions;
+using TaskFlow.Domain.Interfaces.Repository;
+using TaskFlow.Domain.Interfaces.Services;
+
+namespace TaskFlow.Application.UseCases.Implementations
+{
+    public class UserUseCase : IUserUseCase
+    {
+        private readonly IUserRepository _repository;
+        private readonly IPassWordService _passWordService;
+
+        public UserUseCase(IUserRepository repository, IPassWordService passWordService)
+        {
+            _repository = repository;
+            _passWordService = passWordService;
+        }
+
+        public async Task<long> CreateUser(UserRequestDto request, CancellationToken token)
+        {
+            try
+            {
+                var passWordHash = _passWordService.Hash(request.Password);
+
+                var user = new User(request.Name, request.Email, passWordHash);
+
+                return await _repository.InsertAsync(user, token);
+            }
+            catch (Exception ex)
+            {
+                throw new InternalErrorException("Erro ao criar: ", ex.InnerException);
+            }
+        }
+
+        public async Task<UserResponseDto> GetUserById(long id, CancellationToken token)
+        {
+            if (id == 0)
+                throw new BadRequestException("A Id deve ser um valor válido");
+
+            var user = await _repository.GetByIdAsync(id, token) 
+                ?? throw new NotFoundException("Usuário não encontrado");
+
+            return user.ToDto();
+        }
+    }
+}
---
+++ Application/UseCases/Interfaces/IUserUseCase.cs
@@ -0,0 +1,24 @@
+﻿using TaskFlow.Aplication.Dtos;
+using TaskFlow.Application.Dtos;
+
+namespace TaskFlow.Application.UseCases.Interfaces
+{
+    public interface IUserUseCase
+    {
+        /// <summary>
+        /// Recupera os dados de um usuário com base no seu ID.
+        /// </summary>
+        /// <param name="id">ID do usuário a ser consultado.</param>
+        /// <param name="token">Token de cancelamento.</param>
+        /// <returns>Objeto com as informações do usuário.</returns>
+        Task<UserResponseDto> GetUserById(long id, CancellationToken token);
+
+        /// <summary>
+        /// Cria um novo usuário no sistema.
+        /// </summary>
+        /// <param name="request">Objeto contendo os dados do usuário a ser criado.</param>
+        /// <param name="token">Token de cancelamento.</param>
+        /// <returns>ID do usuário recém-criado.</returns>
+        Task<long> CreateUser(UserRequestDto request, CancellationToken token);
+    }
+}

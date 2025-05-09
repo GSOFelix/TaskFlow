565271ab9dbe1f85286ffbf8ae8c08ad8c0e29c8
GSOFelix <guilhermedesoares@gmail.com>
2025-05-09 11:43:58 -0300

    feat: Criar varivel global se string de conexao com Postgres

---
+++ Configurations/ApiConfigurations.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskFlow.Configurations
+{
+    public static class ApiConfigurations
+    {
+        public static string? DbConnection { get;} = Environment.GetEnvironmentVariable("DB_CONNECTION");
+    }
+}
---
+++ Extensions/ApiExtension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskFlow.Configurations;
+using TaskFlow.Context;
+using TaskFlow.Domain.Interfaces.Repository;
+using TaskFlow.Domain.Interfaces.Services;
+using TaskFlow.Repositories;
+using TaskFlow.Services;
+
+namespace TaskFlow.Extensions
+{
+    public static class ApiExtension
+    {
+        public static IServiceCollection AddDependeceInjection(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Adicionar configuraçao com postgres
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(ApiConfigurations.DbConnection,
+            b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+            // injetar dependencia de repositorios
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            services.AddScoped<IMainTaskRepository, MainTaskRepository>();
+            services.AddScoped<ISubTaskRepository, SubTaskRepository>();
+            services.AddScoped<ITaskAssigneeRepository, TaskAssigneeRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            // injetar dependencia de serviços 
+            services.AddScoped<IPassWordService, PassWordService>();
+
+            return services;
+        }
+    }
+}

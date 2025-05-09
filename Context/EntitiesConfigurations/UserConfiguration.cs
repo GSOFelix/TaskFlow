da433a8e3a0cfa23d1bbdf3b0f3b0a61c4877a80
GSOFelix <guilhermedesoares@gmail.com>
2025-05-09 11:12:12 -0300

    perf: aumentar caracteres limite da propriedade nome

--- Context/EntitiesConfigurations/UserConfiguration.cs
+++ Context/EntitiesConfigurations/UserConfiguration.cs
@@ -10,7 +10,7 @@ namespace TaskFlow.Context.EntitiesConfigurations
         {
             builder.HasKey(x => x.Id);
 
-            builder.Property(x => x.Name).IsRequired().HasMaxLength(128);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(155);
             builder.Property(x => x.Email).IsRequired();
             builder.Property(x => x.PasswordHash).IsRequired();
             builder.Property(x => x.CreatAt).IsRequired().HasColumnType("TIMESTAMP WITH TIME ZONE");

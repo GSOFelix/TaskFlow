69ed59f7dd44ba61d72b5377bc8f6ae3d623dbf9
GSOFelix <guilhermedesoares@gmail.com>
2025-05-09 17:29:40 -0300

    feat: criar Dtos de response e request

---
+++ Application/Dtos/UserResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using TaskFlow.Domain.Entities;
+
+namespace TaskFlow.Application.Dtos
+{
+    public record UserResponseDto(
+        long Id,
+        string Name,
+        string Email,
+        DateTime CreatAt
+    );
+
+   
+}

5960c1e272b15d9eb3204d9f1f64b99c1ad97058
GSOFelix <guilhermedesoares@gmail.com>
2025-05-09 17:30:35 -0300

    feat: criar metodo demapeamento de User para Dto

---
+++ Application/Mappers/UserMappers.cs
@@ -0,0 +1,14 @@
+﻿using TaskFlow.Application.Dtos;
+using TaskFlow.Domain.Entities;
+
+namespace TaskFlow.Application.Mappers
+{
+    public static class UserMappers
+    {
+        public static UserResponseDto ToDto(this User user)
+        {
+            return new UserResponseDto(
+                user.Id, user.Name, user.Email, user.CreatAt.ToLocalTime());
+        }
+    }
+}

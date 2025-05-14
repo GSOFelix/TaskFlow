f8ccbecc92ce91638591294add6d226117d81b41
GSOFelix <guilhermedesoares@gmail.com>
2025-05-14 17:32:56 -0300

    feat: criar dto para retorno de erro padrão

---
+++ Application/Dtos/ErrorDto/ErroResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskFlow.Application.Dtos.ErrorDto
+{
+    public record ErroResponseDto
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string Details { get; set; } = string.Empty;
+        public string Path { get; set; } = string.Empty;
+
+    }
+}

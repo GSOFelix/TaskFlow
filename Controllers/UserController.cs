ed9c3470fafdfc33d4f41feb89323820a1d07bf5
GSOFelix <guilhermedesoares@gmail.com>
2025-05-14 17:46:14 -0300

    feat: incluir permissões nas rotas de Usuário

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,20 +1,26 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TaskFlow.Application.Dtos.UserDto;
 using TaskFlow.Application.UseCases.Interfaces;
+using TaskFlow.Extensions.Middleware;
 
 namespace TaskFlow.Controllers
 {
     [Route("api/v1/[controller]")]
     [ApiController]
+    [Authorize]
+
     public class UserController(IUserUseCase userUseCase) : ControllerBase
     {
+        [AuthorizePermission(["search_user"])]
         [HttpGet("{id}")]
         public async Task<ActionResult> Select([FromRoute] long id, CancellationToken token)
         {
             return Ok(await userUseCase.GetUserById(id, token));
         }
 
+        [AuthorizePermission(["create_user"])]
         [HttpPost]
         public async Task<ActionResult> Create([FromBody] UserRequestDto requestDto, CancellationToken token)
         {

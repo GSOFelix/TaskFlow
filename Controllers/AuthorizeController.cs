fda49dbda1eb79e4b472a36bfe8ab69fee3300e4
GSOFelix <guilhermedesoares@gmail.com>
2025-05-14 17:44:32 -0300

    feat: criar controller de autenticação

---
+++ Controllers/AuthorizeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskFlow.Application.Dtos.AuthDto;
+using TaskFlow.Application.UseCases.Interfaces;
+
+namespace TaskFlow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizeController(IAuthorizationUser authorization) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> Login(AuthRequestDto authRequest,CancellationToken token)
+        {
+            var response = await authorization.AuthenticateUser(authRequest, token);
+
+            return Ok(response);
+        }
+    }
+}

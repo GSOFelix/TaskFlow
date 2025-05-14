41f2b2fadde9f5217153ea960e0bd85e8f5dba4d
GSOFelix <guilhermedesoares@gmail.com>
2025-05-14 08:12:05 -0300

    feat: criar controllers de Assigneee Comments

---
+++ Controllers/AssigneeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskFlow.Application.Dtos.TaskAssigneeDto;
+using TaskFlow.Application.UseCases.Interfaces;
+
+namespace TaskFlow.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AssigneeController(ITaskAssigneeUseCase useCase) : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetAll([FromRoute] long id, CancellationToken token)
+        {
+            return Ok(await useCase.GetAllDesignationsByUser(id, token));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateNew([FromBody] TaskAssigneeRequestDto request, CancellationToken token)
+        {
+            await useCase.AssignUserToTask(request, token);
+
+            return Created("", "Vinculado com sucesso");
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete([FromBody] TaskAssigneeRequestDto request, CancellationToken token)
+        {
+            await useCase.UnlinkUserToTask(request, token);
+            return Ok("Desvinculado com sucesso");
+        }
+    }
+}
---
+++ Controllers/CommentsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskFlow.Application.Dtos.CommentsDto;
+using TaskFlow.Application.UseCases.Interfaces;
+
+namespace TaskFlow.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CommentsController(ICommentsUseCase useCase) : ControllerBase
+    {
+        [HttpGet("{commentId}")]
+        public async Task<ActionResult> GetCommentById([FromRoute] long commentId, CancellationToken token)
+        {
+            var comment = await useCase.GetCommentById(commentId, token);
+            return Ok(comment);
+        }
+
+        [HttpGet("task/{maintaskId}")]
+        public async Task<ActionResult> GetCommentsByMainTaskId([FromRoute] long maintaskId, CancellationToken token)
+        {
+            var comments = await useCase.GetCommentsByMainTaskId(maintaskId, token);
+            return Ok(comments);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateNewComment([FromBody] CommentsRequestDto request, CancellationToken token)
+        {
+            var comment = await useCase.PostComment(request, token);
+
+            return CreatedAtAction(nameof(GetCommentById), new { commentId = comment }, "Criado comsucesso");
+        }
+
+        [HttpPatch]
+        public async Task<ActionResult> UpdateComment([FromBody] UpdateCommentRequestDto request, CancellationToken token)
+        {
+            var comment = await useCase.UpdateComment(request, token);
+            return Ok(comment);
+        }
+
+        [HttpDelete("{commentId}/user/{userId}")]
+        public async Task<ActionResult> DeleteComment([FromRoute] long commentId, [FromRoute] long userId, CancellationToken token)
+        {
+            await useCase.DeleteComment(commentId, userId, token);
+            return Ok("Deletado com sucesso");
+        }
+    }
+}

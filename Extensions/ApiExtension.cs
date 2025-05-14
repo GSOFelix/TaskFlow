ff54212a3621465c5392f0270c5085ad9540dc5a
GSOFelix <guilhermedesoares@gmail.com>
2025-05-14 17:39:54 -0300

    feat: configurar extenção para swagger aceitar o token

--- Extensions/ApiExtension.cs
+++ Extensions/ApiExtension.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
 using TaskFlow.Application.UseCases.Implementations;
 using TaskFlow.Application.UseCases.Interfaces;
 using TaskFlow.Configurations;
@@ -7,12 +9,15 @@ using TaskFlow.Domain.Interfaces.Services;
 using TaskFlow.Infra.Context;
 using TaskFlow.Infra.Repositories;
 using TaskFlow.Infra.Services;
+using Microsoft.IdentityModel.Tokens;
+using TaskFlow.Application.Auth;
+using Microsoft.OpenApi.Models;
 
 namespace TaskFlow.Extensions
 {
     public static class ApiExtension
     {
-        public static IServiceCollection AddDependeceInjection(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddDependeceInjection(this IServiceCollection services)
         {
             // Adicionar configuraçao com postgres
             services.AddDbContext<AppDbContext>(options =>
@@ -28,14 +33,81 @@ namespace TaskFlow.Extensions
 
             // injetar dependencia de serviços 
             services.AddScoped<IPassWordService, PassWordService>();
+            services.AddScoped<ITokenService, TokenService>();
 
             // Injetar dependencia de UserCase
             services.AddScoped<IUserUseCase, UserUseCase>();
             services.AddScoped<IMainTaskUseCase, MainTaskUseCase>();
-            services.AddScoped<ITaskAssigneeUseCase,TaskAssigneeUseCase>();
-            services.AddScoped<ICommentsUseCase, CommentsUseCase>();    
+            services.AddScoped<ITaskAssigneeUseCase, TaskAssigneeUseCase>();
+            services.AddScoped<ICommentsUseCase, CommentsUseCase>();
+            services.AddScoped<IAuthorizationUser,AuthorizationUser>();
 
             return services;
+        }
+
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services)
+        {
+            var key = Encoding.UTF8.GetBytes(ApiConfigurations.JwtKey);
+            services.AddAuthentication(op =>
+            {
+                op.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                op.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = ApiConfigurations.JwtIssuer,
+                        ValidateAudience = true,
+                        ValidAudience = ApiConfigurations.JwtAudience,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ClockSkew = TimeSpan.Zero,
+                    };
+                });
+
+            return services;
+        }
+
+        public static IServiceCollection AddInfraSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "TaskFlow",
+                    Version = "v1"
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JSON Web Token",
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+
+            return services;    
         }
     }
 }

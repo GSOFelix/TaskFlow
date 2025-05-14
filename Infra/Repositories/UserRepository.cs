f719bd0695491ece096ee88f4ff0471f87cc84ca
GSOFelix <guilhermedesoares@gmail.com>
2025-05-14 17:36:12 -0300

    feat: criar metodo de procura por email de usuario

--- Infra/Repositories/UserRepository.cs
+++ Infra/Repositories/UserRepository.cs
@@ -14,9 +14,26 @@ namespace TaskFlow.Infra.Repositories
             _context = context;
         }
 
-        public Task<User?> GetByIdAsync(long id, CancellationToken token)
+        public async Task<User?> GetByEmailAsync(string email, CancellationToken token)
         {
-            return _context.Users.FirstOrDefaultAsync(u => u.Id == id, token);
+            var query = _context.Users
+                 .AsNoTracking()
+                 .Include(x => x.UserPermissions)
+                 .ThenInclude(x => x.Permission)
+                 .Where(x => x.Email == email);
+
+            return await query.FirstOrDefaultAsync(token);
+        }
+
+        public async Task<User?> GetByIdAsync(long id, CancellationToken token)
+        {
+            var query = _context.Users
+                 .AsNoTracking()
+                 .Include(x => x.UserPermissions)
+                 .ThenInclude(x => x.Permission)
+                 .Where(x => x.Id == id);
+
+            return await query.FirstOrDefaultAsync(token);
         }
 
         public async Task<long> InsertAsync(User user, CancellationToken token)

bd2cabe9b6f2229c5a0bca2a72d33a4eb7a5591c
GSOFelix <guilhermedesoares@gmail.com>
2025-05-14 17:41:28 -0300

    feat: criar UseCase para Logar na aplicação

---
+++ Application/UseCases/Implementations/AuthorizationUser.cs
@@ -0,0 +1,41 @@
+﻿using TaskFlow.Application.Auth;
+using TaskFlow.Application.Dtos.AuthDto;
+using TaskFlow.Application.UseCases.Interfaces;
+using TaskFlow.Domain.Exceptions;
+using TaskFlow.Domain.Interfaces.Repository;
+using TaskFlow.Domain.Interfaces.Services;
+
+namespace TaskFlow.Application.UseCases.Implementations
+{
+    public class AuthorizationUser : IAuthorizationUser
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPassWordService _passWordService;
+        private readonly ITokenService _tokenService;
+
+        public AuthorizationUser(IUserRepository userRepository, IPassWordService passWordService, ITokenService tokenService)
+        {
+            _userRepository = userRepository;
+            _passWordService = passWordService;
+            _tokenService = tokenService;
+        }
+
+        public async Task<AuthResponseDto> AuthenticateUser(AuthRequestDto request, CancellationToken token)
+        {
+            // Verifica se o usuário existe 
+            var user = await _userRepository.GetByEmailAsync(request.Email,token) 
+                ?? throw new NotFoundException("Usuario não encontrado.");
+
+            // Verifica se a senha está correta 
+            if (!_passWordService.Verify(request.PassWord, user.PasswordHash))
+                throw new UnauthorizeException("Usuário ou senha incorrtos.");
+
+            // Gera o Token
+            var response = new AuthResponseDto(
+                Email: request.Email,
+                Token: _tokenService.GenerateToken(user));
+
+            return response;
+        }
+    }
+}
---
+++ Application/UseCases/Interfaces/IAuthorizationUser.cs
@@ -0,0 +1,10 @@
+﻿using TaskFlow.Application.Dtos.AuthDto;
+
+namespace TaskFlow.Application.UseCases.Interfaces
+{
+    public interface IAuthorizationUser
+    {
+        Task<AuthResponseDto> AuthenticateUser(AuthRequestDto request,CancellationToken token);
+
+    }
+}

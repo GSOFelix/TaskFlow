92bb291defaf8a0ce5078980c4d616967e0dcdcf
GSOFelix <guilhermedesoares@gmail.com>
2025-05-14 08:21:11 -0300

    feat: incluir implementação  no repositorio de novos metodos da interface

--- Infra/Repositories/TaskAssigneeRepository.cs
+++ Infra/Repositories/TaskAssigneeRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TaskFlow.Application.Dtos.TaskAssigneeDto;
 using TaskFlow.Domain.Entities;
+using TaskFlow.Domain.Exceptions;
 using TaskFlow.Domain.Interfaces.Repository;
 using TaskFlow.Infra.Context;
 
@@ -17,6 +19,24 @@ namespace TaskFlow.Infra.Repositories
         public async Task<IEnumerable<TaskAssignee>> GetAllbyUserAsync(long userId, CancellationToken token)
         {
             return await _context.TaskAssignees.Where(u => u.UserId == userId).ToListAsync(token);
+        }
+
+        public async Task InsertAsync(TaskAssignee taskAssignee, CancellationToken token)
+        {
+            await _context.TaskAssignees.AddAsync(taskAssignee, token);
+            await _context.SaveChangesAsync(token);
+        }
+
+        public async Task DeleteAsync(TaskAssignee taskAssignee, CancellationToken token)
+        {
+            _context.TaskAssignees.Remove(taskAssignee);
+            await _context.SaveChangesAsync(token);
+        }
+
+        public async Task<TaskAssignee?> GetByIdAsync(long mainTaskId, long userId, CancellationToken token)
+        {
+            return await _context.TaskAssignees
+                .FirstOrDefaultAsync(x => x.MainTaskId == mainTaskId && x.UserId == userId);
         }
     }
 }

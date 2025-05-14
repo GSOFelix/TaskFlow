678bbf89d8524fdb48c9351f9df87a42b3d1baf4
GSOFelix <guilhermedesoares@gmail.com>
2025-05-14 08:12:32 -0300

    feat: incluir rota para pesquisa por codigo de tarefa

---
+++ Controllers/MainTaskController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskFlow.Application.Dtos;
+using TaskFlow.Application.UseCases.Interfaces;
+
+namespace TaskFlow.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class MainTaskController(IMainTaskUseCase useCase) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] MainTaskRequestDto request, CancellationToken token)
+        {
+            var response = await useCase.CreateMainTask(request, token);
+
+            return Ok(response);
+        }
+
+        [HttpGet("{userId}/{detail}")]
+        public async Task<ActionResult> SelectAllByUser([FromRoute] long userId, CancellationToken token, [FromRoute] bool detail = false)
+        {
+            return Ok(await useCase.GetAllMainTasks(userId, detail, token));
+        }
+
+        [HttpGet("task/{mainTaskId}/{detail}")]
+        public async Task<ActionResult> SelectByMainTask([FromRoute] long mainTaskId, CancellationToken token, [FromRoute] bool detail = false)
+        {
+            return Ok(await useCase.GetMainTaskById(mainTaskId, detail, token));
+        }
+    }
+}

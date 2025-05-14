bd2de2aa8189d232047aa0be3bc82f60583dcf7b
GSOFelix <guilhermedesoares@gmail.com>
2025-05-14 08:17:37 -0300

    perf: incluir parametro de detail pra retornar detalhes da tarefa, melhorando o desenpenho da rota

--- Domain/Interfaces/Repository/IMainTaskRepository.cs
+++ Domain/Interfaces/Repository/IMainTaskRepository.cs
@@ -8,17 +8,19 @@ namespace TaskFlow.Domain.Interfaces.Repository
         /// Retorna todas as tarefas pertencentes a um usuário específico.
         /// </summary>
         /// <param name="userId">ID do usuário dono das tarefas.</param>
+        /// <param name="detail">Detalha os comentarios e pessoas destinadas a tarefa</param>
         /// <param name="token">Token de cancelamento</param>
         /// <returns>Lista de tarefas do usuário.</returns>
-        Task<IEnumerable<MainTask>> GetAllByUserAsync(long userId,CancellationToken token);
+        Task<IEnumerable<MainTask>> GetAllByUserAsync(long userId, bool detail,CancellationToken token);
 
         /// <summary>
         /// Retorna uma tarefa específica pelo seu ID.
         /// </summary>
         /// <param name="mainTaskId">ID da tarefa principal.</param>
+        /// <param name="detail">Detalha os comentarios e pessoas destinadas a tarefa</param>
         /// <param name="token">Token de cancelamento</param>
         /// <returns>A tarefa correspondente, se existir.</returns>
-        Task<MainTask?> GetByIdAsync(long mainTaskId, CancellationToken token);
+        Task<MainTask?> GetByIdAsync(long mainTaskId,bool detail ,CancellationToken token);
 
         /// <summary>
         /// Insere uma nova tarefa no banco de dados.

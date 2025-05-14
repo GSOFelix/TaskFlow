7333605e79348c81ba7c00167736255f26f0d34b
GSOFelix <guilhermedesoares@gmail.com>
2025-05-14 17:40:57 -0300

    feat: crir variveis para configurar jwt

--- Configurations/ApiConfigurations.cs
+++ Configurations/ApiConfigurations.cs
@@ -2,6 +2,13 @@
 {
     public static class ApiConfigurations
     {
-        public static string? DbConnection { get;} = Environment.GetEnvironmentVariable("DB_CONNECTION");
+        public static string DbConnection { get; } = Environment.GetEnvironmentVariable("DB_CONNECTION")
+            ?? throw new InvalidOperationException("String de conexão não esta definida nas variáves de ambiente.");
+
+        public static string JwtKey { get; } = Environment.GetEnvironmentVariable("JWTKEY")
+            ?? throw new InvalidOperationException("JWTKEY não esta definida nas variáves de ambiente.");
+
+        public const string JwtIssuer = "TaskFlowAPI";
+        public const string JwtAudience = "TaskFlowClient";
     }
 }
